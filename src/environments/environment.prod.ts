77bab92cf826d65c0cfeca846ed9f26c9740537c
Pedro Vila <vilapedro92@gmail.com>
2022-12-14 10:07:54 -0300

    Added hex architecture in angular proyect

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+  },
+  baseURL: "http://localhost:8080/api/v1/",
+  endpoints: {},
+  storage: {}
+};
